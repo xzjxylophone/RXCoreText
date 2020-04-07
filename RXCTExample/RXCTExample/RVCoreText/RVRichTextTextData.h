852ed6455a0c2ef09b1c2cd4768674d3a747ce7b
Rush.D.xzj <xzjxylophone@gmail.com>
2020-04-07 20:54:23 +0800

    fixed

--- RXCTExample/RXCTExample/RVCoreText/RVRichTextTextData.h
+++ RXCTExample/RXCTExample/RVCoreText/RVRichTextTextData.h
@@ -16,8 +16,12 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy) NSString *value;
 @property (nonatomic, assign) NSInteger font;
 @property (nonatomic, assign) NSString *color;
-@property (nonatomic, assign) NSInteger lineSpace;
+@property (nonatomic, assign) NSInteger lineSpace;   // 行间距
+@property (nonatomic, assign) NSInteger numberSpace; // 字间距
 @property (nonatomic, assign) NSInteger height;
+@property (nonatomic, assign) NSInteger top;
+@property (nonatomic, assign) NSInteger left;
+@property (nonatomic, assign) NSInteger right;
 
 @end
 
--- RXCTExample/RXCTExample/RVCoreText/RVRichTextView.m
+++ RXCTExample/RXCTExample/RVCoreText/RVRichTextView.m
@@ -9,8 +9,14 @@
 #import "RVRichTextView.h"
 #import <CoreText/CoreText.h>
 #import "Masonry.h"
+#import "RVRichTextTextData.h"
+#import "MJExtension.h"
+#import "UIColor+RXUtility.h"
+
 
 @interface RVRichTextView ()
+
+@property (nonatomic, copy) NSArray *dataArray;
 
 @end
 
@@ -19,8 +25,13 @@
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         NSDictionary *dict = [self _foramtSDKStringWithFileName:@"rv_core_text"];
-        NSLog(@"dict:%@", dict);
-        
+        NSArray *dataDictArray = dict[@"data"];
+        NSMutableArray *dataArray = [NSMutableArray new];
+        if (dataDictArray.count > 0) {
+            RVRichTextTextData *textData  = [RVRichTextTextData mj_objectWithKeyValues:dataDictArray.firstObject];
+            [dataArray addObject:textData];
+        }
+        self.dataArray = dataArray;
         self.backgroundColor = [UIColor clearColor];
         self.layer.borderColor = [UIColor blackColor].CGColor;
         self.layer.borderWidth = 1.0f;
@@ -41,6 +52,9 @@
 - (void)drawRect:(CGRect)rect {
     [super drawRect:rect];
     
+    if (self.dataArray.count == 0) {
+        return;
+    }
     //获取当前上下文
     CGContextRef context = UIGraphicsGetCurrentContext();
     //翻转坐标系步骤
@@ -50,15 +64,14 @@
     CGContextTranslateCTM(context, 0, self.bounds.size.height);
     //文本翻转成为CoreText坐标系
     CGContextScaleCTM(context, 1, -1);
-    
-    
     CTFrameRef frame = NULL;
-    frame = [self vvv1];
+    
+    frame = [self vvv_custom];
+//    frame = [self vvv1];
+//    frame = [self vvv1_1];
+    
 //    frame = [self vvv2];
-    
     CTFrameDraw(frame, context);
-    
-    
 }
 
 
@@ -70,26 +83,236 @@
         tmp.layer.masksToBounds = YES;
         tmp.layer.borderWidth = 1.0;
         tmp.tag = tag;
-        tmp.backgroundColor = [UIColor greenColor];
+        tmp.backgroundColor = [UIColor clearColor];
         [self addSubview:tmp];
     }
 }
 
 
+
+- (CTFrameRef)vvv_custom {
+    
+    
+
+    CGFloat height = self.frame.size.height;
+    CGFloat width = self.frame.size.width;
+    /*for (NSInteger i = 0; i < self.dataArray.count; i++)*/
+    {
+        
+        
+        
+        RVRichTextTextData *textData = self.dataArray.firstObject;
+        
+        
+        [self addViewIfNecessaryWithRect:CGRectMake(textData.left, textData.top, width - textData.left - textData.right, textData.height) tag:11111];
+        
+        CGRect rect = CGRectMake(textData.left, height - textData.height - textData.top, width - textData.left - textData.right, textData.height);
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathAddRect(path, NULL, rect);
+        
+        NSInteger strLength = textData.value.length;
+        NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:textData.value];
+        
+        UIColor *textColor = [UIColor rx_colorWithString:[NSString stringWithFormat:@"#%@", textData.color]];
+//        textColor = [UIColor redColor];
+        NSRange range = NSMakeRange(0 , strLength);
+        //设置文本内容的属性
+        //1设置部分文字颜色
+        [attString addAttribute:(id)kCTForegroundColorAttributeName value:textColor range:range];
+        //2设置部分文字字体
+        CGFloat fontSize = textData.font;
+        CTFontRef fontRef = CTFontCreateWithName((CFStringRef)@"ArialMT", fontSize, NULL);
+        [attString addAttribute:(id)kCTFontAttributeName value:(__bridge id)fontRef range:range];
+        
+        //3设置斜体
+//        CTFontRef italicFontRef = CTFontCreateWithName((CFStringRef)[UIFont italicSystemFontOfSize:20].fontName, 16, NULL);
+//        [attString addAttribute:(id)kCTFontAttributeName value:(__bridge id)italicFontRef range:NSMakeRange(27, 9)];
+//        // xzj_todo
+//        [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(27 , 9)];
+        
+        //4设置下划线
+//        [attString addAttribute:(id)kCTUnderlineStyleAttributeName value:(id)[NSNumber numberWithInteger:kCTUnderlineStyleDouble] range:NSMakeRange(36, 10)];
+//        //5设置下划线颜色
+//        [attString addAttribute:(id)kCTUnderlineColorAttributeName value:(id)[UIColor greenColor].CGColor range:NSMakeRange(36, 10)];
+//        // xzj_todo
+//        [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(36 , 10)];
+        
+        //6设置空心字
+//        long number1 = 2;
+//        CFNumberRef numRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &number1);
+//        [attString addAttribute:(id)kCTStrokeWidthAttributeName value:(__bridge id)numRef range:NSMakeRange(56, 10)];
+//        // xzj_todo
+//        [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor orangeColor] range:NSMakeRange(56 , 10)];
+        
+        
+        
+        //7设置字体间距
+        long number = textData.numberSpace;
+        CFNumberRef num = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &number);
+        [attString addAttribute:(id)kCTKernAttributeName value:(__bridge id)num range:range];
+        // xzj_todo
+//        [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor purpleColor] range:range];
+        
+        //8设置行间距
+        CGFloat lineSpacing = textData.lineSpace;
+        const CFIndex kNumberOfSettings = 3;
+        CTParagraphStyleSetting theSettings[kNumberOfSettings] = {
+            {kCTParagraphStyleSpecifierLineSpacingAdjustment, sizeof(CGFloat), &lineSpacing},
+            {kCTParagraphStyleSpecifierMaximumLineHeight, sizeof(CGFloat), &lineSpacing},
+            {kCTParagraphStyleSpecifierMinimumLineHeight, sizeof(CGFloat), &lineSpacing}
+        };
+        CTParagraphStyleRef theParagraphRef = CTParagraphStyleCreate(theSettings, kNumberOfSettings);
+        [attString addAttribute:(id)kCTParagraphStyleAttributeName value:(__bridge id)theParagraphRef range:NSMakeRange(0, [attString length])];
+        
+        CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
+        CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, [attString length]), path, NULL);
+        return frame;
+    }
+    
+    return nil;
+    
+//    CGRect rect = CGRectMake(0, 0, self.frame.size.width, 300);
+////     [self addViewIfNecessaryWithRect:rect tag:11111];
+//
+//     //1.创建绘制区域,显示的区域可以用CGMUtablePathRef生成任意的形状
+//    CGMutablePathRef path = CGPathCreateMutable();
+//    CGPathAddRect(path, NULL, rect);
+//
+//    //设置绘制的文本内容
+//     NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:@"\tWhen I will learn CoreText, i think it will hard for me.But it is easy.\n\tIn fact,if you bengin learn, you can know that every thing is easy when you start.you just need some knowledge"];
+//    //设置文本内容的属性
+//    //1设置部分文字颜色
+//    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(0 , 27)];
+//    //2设置部分文字字体
+//    CGFloat fontSize = 20;
+//    CTFontRef fontRef = CTFontCreateWithName((CFStringRef)@"ArialMT", fontSize, NULL);
+//    [attString addAttribute:(id)kCTFontAttributeName value:(__bridge id)fontRef range:NSMakeRange(0, 27)];
+//
+//    //3设置斜体
+//    CTFontRef italicFontRef = CTFontCreateWithName((CFStringRef)[UIFont italicSystemFontOfSize:20].fontName, 16, NULL);
+//    [attString addAttribute:(id)kCTFontAttributeName value:(__bridge id)italicFontRef range:NSMakeRange(27, 9)];
+//    // xzj_todo
+//    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(27 , 9)];
+//
+//    //4设置下划线
+//    [attString addAttribute:(id)kCTUnderlineStyleAttributeName value:(id)[NSNumber numberWithInteger:kCTUnderlineStyleDouble] range:NSMakeRange(36, 10)];
+//    //5设置下划线颜色
+//    [attString addAttribute:(id)kCTUnderlineColorAttributeName value:(id)[UIColor greenColor].CGColor range:NSMakeRange(36, 10)];
+//    // xzj_todo
+//    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(36 , 10)];
+//
+//    //6设置空心字
+//    long number1 = 2;
+//    CFNumberRef numRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &number1);
+//    [attString addAttribute:(id)kCTStrokeWidthAttributeName value:(__bridge id)numRef range:NSMakeRange(56, 10)];
+//    // xzj_todo
+//    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor orangeColor] range:NSMakeRange(56 , 10)];
+//
+//
+//
+//    //7设置字体间距
+//    long number = 10;
+//    CFNumberRef num = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &number);
+//    [attString addAttribute:(id)kCTKernAttributeName value:(__bridge id)num range:NSMakeRange(40, 10)];
+//    // xzj_todo
+//    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor purpleColor] range:NSMakeRange(40 , 10)];
+//
+//    //8设置行间距
+//    CGFloat lineSpacing = 10;
+//    const CFIndex kNumberOfSettings = 3;
+//    CTParagraphStyleSetting theSettings[kNumberOfSettings] = {
+//        {kCTParagraphStyleSpecifierLineSpacingAdjustment, sizeof(CGFloat), &lineSpacing},
+//        {kCTParagraphStyleSpecifierMaximumLineHeight, sizeof(CGFloat), &lineSpacing},
+//        {kCTParagraphStyleSpecifierMinimumLineHeight, sizeof(CGFloat), &lineSpacing}
+//    };
+//    CTParagraphStyleRef theParagraphRef = CTParagraphStyleCreate(theSettings, kNumberOfSettings);
+//    [attString addAttribute:(id)kCTParagraphStyleAttributeName value:(__bridge id)theParagraphRef range:NSMakeRange(0, [attString length])];
+//
+//    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
+//    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, [attString length]), path, NULL);
+//    return frame;
+}
+
+
+
+
 // https://www.cnblogs.com/purple-sweet-pottoes/p/5109413.html
 - (CTFrameRef)vvv1 {
-    
     CGRect rect = CGRectMake(0, 0, self.frame.size.width, 40);
     [self addViewIfNecessaryWithRect:rect tag:11111];
-    
     //1.创建绘制区域,显示的区域可以用CGMUtablePathRef生成任意的形状
-   CGMutablePathRef path = CGPathCreateMutable();
-   CGPathAddRect(path, NULL, rect);
-   //2.创建需要绘制的文字
-   NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:@"\tWhen I will learn CoreText, i think it will hard for me.But it is easy.\n\tIn fact,if you bengin learn, you can know that every thing is easy when you start.you just need some knowledge"];
-   //3.根据AttString生成CTFramesetterRef
-   CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
-   CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, [attString length]), path, NULL);
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, rect);
+    //2.创建需要绘制的文字
+    NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:@"\tWhen I will learn CoreText, i think it will hard for me.But it is easy.\n\tIn fact,if you bengin learn, you can know that every thing is easy when you start.you just need some knowledge"];
+    //3.根据AttString生成CTFramesetterRef
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, [attString length]), path, NULL);
+    return frame;
+}
+
+// 1和1_1的代码来源是同一个网址
+- (CTFrameRef)vvv1_1 {
+
+     CGRect rect = CGRectMake(0, 0, self.frame.size.width, 300);
+//     [self addViewIfNecessaryWithRect:rect tag:11111];
+     
+     //1.创建绘制区域,显示的区域可以用CGMUtablePathRef生成任意的形状
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, rect);
+    
+    //设置绘制的文本内容
+     NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:@"\tWhen I will learn CoreText, i think it will hard for me.But it is easy.\n\tIn fact,if you bengin learn, you can know that every thing is easy when you start.you just need some knowledge"];
+    //设置文本内容的属性
+    //1设置部分文字颜色
+    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(0 , 27)];
+    //2设置部分文字字体
+    CGFloat fontSize = 20;
+    CTFontRef fontRef = CTFontCreateWithName((CFStringRef)@"ArialMT", fontSize, NULL);
+    [attString addAttribute:(id)kCTFontAttributeName value:(__bridge id)fontRef range:NSMakeRange(0, 27)];
+    
+    //3设置斜体
+    CTFontRef italicFontRef = CTFontCreateWithName((CFStringRef)[UIFont italicSystemFontOfSize:20].fontName, 16, NULL);
+    [attString addAttribute:(id)kCTFontAttributeName value:(__bridge id)italicFontRef range:NSMakeRange(27, 9)];
+    // xzj_todo
+    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(27 , 9)];
+    
+    //4设置下划线
+    [attString addAttribute:(id)kCTUnderlineStyleAttributeName value:(id)[NSNumber numberWithInteger:kCTUnderlineStyleDouble] range:NSMakeRange(36, 10)];
+    //5设置下划线颜色
+    [attString addAttribute:(id)kCTUnderlineColorAttributeName value:(id)[UIColor greenColor].CGColor range:NSMakeRange(36, 10)];
+    // xzj_todo
+    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(36 , 10)];
+    
+    //6设置空心字
+    long number1 = 2;
+    CFNumberRef numRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &number1);
+    [attString addAttribute:(id)kCTStrokeWidthAttributeName value:(__bridge id)numRef range:NSMakeRange(56, 10)];
+    // xzj_todo
+    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor orangeColor] range:NSMakeRange(56 , 10)];
+    
+    
+    
+    //7设置字体间距
+    long number = 10;
+    CFNumberRef num = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &number);
+    [attString addAttribute:(id)kCTKernAttributeName value:(__bridge id)num range:NSMakeRange(40, 10)];
+    // xzj_todo
+    [attString addAttribute:(id)kCTForegroundColorAttributeName value:[UIColor purpleColor] range:NSMakeRange(40 , 10)];
+    
+    //8设置行间距
+    CGFloat lineSpacing = 10;
+    const CFIndex kNumberOfSettings = 3;
+    CTParagraphStyleSetting theSettings[kNumberOfSettings] = {
+        {kCTParagraphStyleSpecifierLineSpacingAdjustment, sizeof(CGFloat), &lineSpacing},
+        {kCTParagraphStyleSpecifierMaximumLineHeight, sizeof(CGFloat), &lineSpacing},
+        {kCTParagraphStyleSpecifierMinimumLineHeight, sizeof(CGFloat), &lineSpacing}
+    };
+    CTParagraphStyleRef theParagraphRef = CTParagraphStyleCreate(theSettings, kNumberOfSettings);
+    [attString addAttribute:(id)kCTParagraphStyleAttributeName value:(__bridge id)theParagraphRef range:NSMakeRange(0, [attString length])];
+    
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, [attString length]), path, NULL);
     return frame;
 }
 
